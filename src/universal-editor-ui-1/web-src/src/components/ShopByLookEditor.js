f9eee697c5fefdc0b428397daa1591f96865f6ce
sumankmaityacen <suman.k.maity@accenture.com>
2024-07-31 12:26:38 +0530

    shopbylook added and removed commented codes

--- src/universal-editor-ui-1/web-src/src/components/ShopByLookEditor.js
+++ src/universal-editor-ui-1/web-src/src/components/ShopByLookEditor.js
@@ -15,9 +15,8 @@ import {
   import { attach } from "@adobe/uix-guest";
   import React, { useEffect, useState } from "react";
   import axios from 'axios';
-  import { shopByLookExtensionID,SERIES,MARKET_SEGMENT } from "./Constants";
-  
-  const PRECON_MODEL_API_URL = `https://productdata.api.bmw/pdh/precons/v1.0/${SERIES}/${MARKET_SEGMENT}`;
+  import { shopByLookExtensionID,SERIES,PRECON_BASE_URL,RANGE,VEHICLE,VEHICLES} from "./Constants";
+  import actions from '../config.json';
   
   export default function () {
     const [guestConnection, setGuestConnection] = useState();
@@ -36,53 +35,103 @@ import {
 
     const [vehicleTypeData, setVehicleTypeData] = useState([]);
     const [selectedVehicleType, setSelectedVehicleType] = useState('');
+    const [model, setModel] = useState([]);
 
     const [selected, setSelected] = useState(false);
+    const[tenant, setTenant] =useState('');
+    const[error, setError] =useState(null);
+
+    const SHOPBYLOOK_MODEL_API_URL = `${PRECON_BASE_URL}${SERIES}/${tenant}`;
+
+    useEffect(() => {
+      (async () => {
+        const connection = await attach({ id: shopByLookExtensionID })
+        setGuestConnection(connection);
+      })()
+    }, [])
+
+    useEffect(() => {
+      const extensionCORS = async () => {
+        try {
+          if(guestConnection){
+            const state = await guestConnection.host.editorState.get();
+            const token = await guestConnection.sharedContext.get('token');
+            const org = await guestConnection.sharedContext.get('orgId');
+            const location = new URL(state.location);
+            const builtHeaders = {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`,
+                'x-aem-host': location.protocol + '//' + location.host,
+                'x-gw-ims-org-id': org,
+            };
+            const response = await fetch(actions["get-metadata"], {
+              method: 'POST',
+              headers: builtHeaders,
+              body: JSON.stringify({ url: location.pathname })
+            });
+            const responseData = await response.json();
+            setTenant(responseData.tenant)
+          }
+         
+        } catch (error) {
+          setError(error);
+        }
+         finally {
+          setLoading(false);
+        }
+      }
+      extensionCORS();
+    },[shopByLookExtensionID,guestConnection]);
   
     const onCarSeriesChangeHandler = (value) => {
-      console.log("onChange on extension side", value);
       setSelectedCarSeries(value);
       setSeriesCode(value);
       setSelected(true);
-      setCarModelRange([]); // Clear car model range options
-      setSelectedCarModelRange(''); // Clear selected car model range
-      localStorage.setItem('selectedCarSeries', value);
-      localStorage.removeItem('selectedCarModelRange');
-      guestConnection?.host?.field.onChange(value);
+      guestConnection?.host?.field.onChange(`${value} `);
     };
     
     const onCarModelRangeChangeHandler = (value) => {
-      console.log("onChange on extension side", value);
       setSelectedCarModelRange(value);
       setRangeCode(value);
-      setVehicleTypeData([]); // Clear vehicle type data
-    setSelectedVehicleType(''); 
-    localStorage.setItem('selectedCarModelRange', value);
-    localStorage.removeItem('selectedVehicleType');
       guestConnection?.host?.field.onChange(`${selectedCarSeries}, ${value}`);
     };
   
     const onVehicleChangeHandler = (value) => {
-      console.log("onChange on extension side", value);
       setSelectedVehicleType(value);
-    localStorage.setItem('selectedVehicleType', value);
-      guestConnection?.host?.field.onChange(`${selectedCarSeries}, ${selectedCarModelRange}, ${value}`);
+      const shopId = value?.slice(0, 4);
+      guestConnection?.host?.field.onChange(`${seriesCode}, ${selectedCarSeries}, ${selectedCarModelRange}, ${shopId}, ${value}`);
     };
+
+    useEffect(() => {
+      const getDataValue = async () => {
+        if(guestConnection){
+          const modelData = await guestConnection.host.field.getValue();
+          setModel([modelData]);
+          if (modelData) {
+            const [seriesCode, modelRange, ...type] = modelData.split(', ');
+            setSeriesCode(seriesCode.split(',')[0]);
+            setSelectedCarSeries(seriesCode.split(',')[1]);
+            setSelected(true);
+            setRangeCode(modelRange);
+            setSelectedCarModelRange(modelRange);
+            setShopbylookID([type[0]]);
+            setVehicleTypeData(type);
+            setSelectedVehicleType(type.join(', '));
+          }
+        }  
+      }
+      getDataValue();
+    }, [guestConnection]);
   
     useEffect(() => {
       const fetchData = async () => {
         try {
-          const response = await fetch(PRECON_MODEL_API_URL);
-          const data = await response.json();
-          const seriesCodes = Object.values(data).map(item => item.seriesCode);
-          setCarSerieses(Object.values(seriesCodes));
-          const savedCarSeries = localStorage.getItem('selectedCarSeries');
-          if (savedCarSeries) {
-            setSelectedCarSeries(savedCarSeries);
-            setSeriesCode(savedCarSeries);
+          if(tenant){
+            const response = await fetch(SHOPBYLOOK_MODEL_API_URL);
+            const data = await response.json();
+            const seriesCodes = Object.values(data).map(item => item.seriesCode);
+            setCarSerieses(Object.values(seriesCodes));
           }
-          const connection = await attach({ id: shopByLookExtensionID });
-          setGuestConnection(connection);
         } catch (error) {
           console.error("Error fetching data:", error);
         } finally {
@@ -91,25 +140,20 @@ import {
       };
   
       fetchData();
-    }, []);
-      //modelrange
-    const URL = `https://productdata.api.bmw/pdh/precons/v1.0/ranges/${MARKET_SEGMENT}`;
+    }, [tenant]);
+      
+    const shopbylookapiURL = `${PRECON_BASE_URL}${RANGE}${tenant}`;
     useEffect(() => {
       const fetchModelRange = async () => {
         if (!seriesCode) return;
         try {
-          const modelDetailUrl = `${URL}/${seriesCode}`;
-          const modelDetailResponse = await axios.get(modelDetailUrl);
-          const modelDetail = modelDetailResponse?.data;
-          const rangeCode = Object.values(modelDetail).map(item => item.modelRangeCode);
-          setCarModelRange(Object.values(rangeCode));
-          const savedCarModelRange = localStorage.getItem('selectedCarModelRange');
-          if (savedCarModelRange) {
-            setSelectedCarModelRange(savedCarModelRange);
+          if(tenant){
+            const modelDetailUrl = `${shopbylookapiURL}/${seriesCode}`;
+            const modelDetailResponse = await axios.get(modelDetailUrl);
+            const modelDetail = modelDetailResponse?.data;
+            const rangeCode = Object.values(modelDetail).map(item => item.modelRangeCode);
+            setCarModelRange(Object.values(rangeCode));
           }
-          rangeCode?.map((item) => setRangeCode(item?.modelRangeCode));
-          const connection = await attach({ id: shopByLookExtensionID });
-          setGuestConnection(connection);
         } catch (error) {
           console.error('Error fetching details for model', error);
         } finally {
@@ -118,19 +162,19 @@ import {
       };
   
       fetchModelRange();
-    }, [seriesCode]);
+    }, [tenant,seriesCode]);
   
-    const SHOPBYLOOK_VEHICLES_API_URL = `https://productdata.api.bmw/pdh/precons/v1.0/vehicles/${MARKET_SEGMENT}`
+    const SHOPBYLOOK_VEHICLES_API_URL = `${PRECON_BASE_URL}${VEHICLES}${tenant}`
     useEffect(() =>{
       const fetchVehicles = async () => {
         if (!rangeCode) return;
         try {
-          const modelDetailUrl = `${SHOPBYLOOK_VEHICLES_API_URL}/${rangeCode}?vehicle_type=SHOP_THE_LOOK`;
-          const response = await axios.get(modelDetailUrl);
-          const shopByLookID = Object.values(response?.data).map(item => item?.id);
-          setShopbylookID(shopByLookID);
-          const connection = await attach({ id: shopByLookExtensionID });
-          setGuestConnection(connection);
+          if(tenant){
+            const modelDetailUrl = `${SHOPBYLOOK_VEHICLES_API_URL}/${rangeCode}?vehicle_type=SHOP_THE_LOOK`;
+            const response = await axios.get(modelDetailUrl);
+            const shopByLookID = Object.values(response?.data).map(item => item?.id);
+            setShopbylookID(shopByLookID);
+          }
         } catch (error) {
           console.error('Error fetching details for model', error);
         } finally {
@@ -139,33 +183,30 @@ import {
       };
   
       fetchVehicles();
-  }, [rangeCode]);
+  }, [tenant,rangeCode]);
   
-  const SHOPBYLOOK_ID_VEHICLE_API_URL = `https://productdata.api.bmw/pdh/precons/v1.0/vehicle/${MARKET_SEGMENT}`
+  const SHOPBYLOOK_ID_VEHICLE_API_URL = `${PRECON_BASE_URL}${VEHICLE}${tenant}`
   useEffect(() =>{
     const fetchVehicleByPreConId = async () => {
       if (!shopbylookID.length) return;
       try {
-        const vehicleDataPromises = shopbylookID.map(async shopbylookID => {
-          const modelDetailUrl = `${SHOPBYLOOK_ID_VEHICLE_API_URL}/${shopbylookID}`;
-          const response = await axios.get(modelDetailUrl);
-          return response.data;
-        });
-        const vehiclesData = await Promise.all(vehicleDataPromises);
-        const vehicles = vehiclesData.map(vehicle => {
-          const { id, name, headline } = vehicle;
-          let parts = [];
-          if (id) parts.push(id);
-          if (name) parts.push(name);
-          if (headline) parts.push(headline);
-          return parts.join(', ');
-        });
-        setVehicleTypeData(vehicles);
-        const savedVehicleTypeData = localStorage.getItem('selectedVehicleType');
-        if (savedVehicleTypeData) setSelectedVehicleType(savedVehicleTypeData);
-  
-        const connection = await attach({ id: priConExtensionId });
-        setGuestConnection(connection);
+        if(tenant){
+          const vehicleDataPromises = shopbylookID.map(async shopbylookID => {
+            const modelDetailUrl = `${SHOPBYLOOK_ID_VEHICLE_API_URL}/${shopbylookID}`;
+            const response = await axios.get(modelDetailUrl);
+            return response.data;
+          });
+          const vehiclesData = await Promise.all(vehicleDataPromises);
+          const vehicles = vehiclesData.map(vehicle => {
+            const { id, name, headline } = vehicle;
+            let parts = [];
+            if (id) parts.push(id);
+            if (headline) parts.push(headline);
+            if (name) parts.push(name);
+            return parts.join('  ');
+          });
+          setVehicleTypeData(vehicles);
+        }
       } catch (error) {
         console.error('Error fetching details for model', error);
       } finally {
@@ -174,28 +215,8 @@ import {
     };
   
     fetchVehicleByPreConId();
-  }, [shopbylookID]);
+  }, [tenant,shopbylookID]);
 
-  useEffect(() => {
-    // Enable dropdowns if values are stored in localStorage
-    const savedCarSeries = localStorage.getItem('selectedCarSeries');
-    const savedCarModelRange = localStorage.getItem('selectedCarModelRange');
-    const savedVehicleType = localStorage.getItem('selectedVehicleType');
-    
-    if (savedCarSeries) {
-      setSelectedCarSeries(savedCarSeries);
-      setSeriesCode(savedCarSeries);
-      setSelected(true);
-    }
-    if (savedCarModelRange) {
-      setSelectedCarModelRange(savedCarModelRange);
-      setRangeCode(savedCarModelRange);
-    }
-    if (savedVehicleType) {
-      setSelectedVehicleType(savedVehicleType);
-    }
-  }, []);
-  
     return (
       <Provider theme={lightTheme} colorScheme="light">
         <Flex direction="column">
