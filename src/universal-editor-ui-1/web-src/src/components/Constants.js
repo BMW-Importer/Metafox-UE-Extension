c17f6929c462aaa1b383a0cd3452d406efac73b3
Amol <amol.y.patil@accenyure.com>
2024-08-02 13:24:40 +0530

    updated api to prod
    updated api to prod

--- src/universal-editor-ui-1/web-src/src/components/Constants.js
+++ src/universal-editor-ui-1/web-src/src/components/Constants.js
@@ -3,7 +3,7 @@ module.exports = {
   priConExtensionId: 'precon-editor',
   shopByLookExtensionID: 'shopbylook-editor',
   techDataExtensionID: 'techdata-editor',
-  BASE_URL : "https://productdata-int1.api.bmw//pdh/categoryhub/v1.0/all/",
+  BASE_URL : "https://productdata.api.bmw//pdh/categoryhub/v1.0/all/",
   MODEL_BASE_URL : "https://productdata.api.bmw/pdh/technicaldata/v2.0/model/",
   LATEST : "/latest",
   PRECON_BASE_URL : "https://productdata.api.bmw/pdh/precons/v1.0",
